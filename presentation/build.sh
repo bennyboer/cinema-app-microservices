f52aa023fbc03aec38f9c18934f405e410a87e12
Benjamin Eder <barny.eder@gmail.com>
2019-06-17 13:11:46 +0200

    Fixed docker

--- presentation/build.sh
+++ presentation/build.sh
@@ -3,6 +3,11 @@ echo "PRESENTATION SERVICE | Initializing environment..."
 export GO111MODULE=on
 
 echo "PRESENTATION SERVICE | Fetching dependencies..."
+if [[ "$1" == "docker" ]]; then
+    git config --global user.name "bennyboer-machine-user"
+    git config --global credential.helper store
+    echo https://51faa31d4b9f08c8e56d4fb23fc082a85e617df8:x-oauth-basic@github.com >> ~/.git-credentials
+fi
 if [[ "$1" == "upgrade" ]]; then
     go get -u
 else
--- reservation/service.dockerfile
+++ reservation/service.dockerfile
@@ -2,7 +2,7 @@ FROM obraun/vss-protoactor-jenkins as builder
 RUN mkdir -p /go/src/github.com/ob-vss-ss19/blatt-4-sudo_blatt4/
 COPY . /go/src/github.com/ob-vss-ss19/blatt-4-sudo_blatt4/
 WORKDIR /go/src/github.com/ob-vss-ss19/blatt-4-sudo_blatt4/reservation
-RUN sh build.sh
+RUN sh build.sh docker
 
 FROM iron/go
 ENTRYPOINT ["/go/src/github.com/ob-vss-ss19/blatt-4-sudo_blatt4/reservation/reservation-service"]
